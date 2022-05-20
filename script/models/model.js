a27c8df85c7feee7b87b9a647a973d6562a5163e
VITOR AUGUSTO ALMEIDA DE MOURA <v.augusto.am@gmail.com>
2022-05-20 19:51:16 -0300

    fix: verify email function
    Função para verificação de email corrigida e melhorada

--- script/models/model.js
+++ script/models/model.js
@@ -6,6 +6,18 @@ class Email{
     verifyLogin(email){
         let user = email.value.substring(0, email.value.indexOf('@'));
         let domain = email.value.substring(email.value.indexOf('@')+1, email.value.length);
-        
+        if ((usuario.length >=1) &&
+            (domain.length >= 3) &&
+            (user.search("@") ==-1) &&
+            (domain.search("@") ==-1) &&
+            (user.search(" ") ==-1) &&
+            (domain.search(" ") ==-1) &&
+            (domain.search(".") != -1) &&
+            (domain.indexOf(".") >=1) &&
+            (domain.lastIndexOf(".") < domain.length -1)){
+                return 'Email válido' 
+            } else {
+                return 'Email inválido' 
+            }
     }
 }
